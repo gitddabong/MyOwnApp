1b920f42b427391d4defd36e599333d3dcf01ac3
gitddabong <wonjong0701@naver.com>
2022-05-25 10:14:20 +0900

    timer 기능 버그 수정

--- app/src/main/java/com/example/myownapp/TimerActivity.kt
+++ app/src/main/java/com/example/myownapp/TimerActivity.kt
@@ -20,6 +20,7 @@ class TimerActivity : AppCompatActivity() {
     private var time = 0
     private var timerTask : Timer? = null
     private var txtTime : TextView? = null
+    private var timerFlag = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,14 +40,20 @@ class TimerActivity : AppCompatActivity() {
 
         val button1 = findViewById<Button>(R.id.activatePip)
         button1.setOnClickListener(View.OnClickListener {
-            enterPictureInPictureMode(pipParamBuilder.build())
+            enterPictureInPictureMode(
+                pipParamBuilder
+                    .setSeamlessResizeEnabled(false)
+                    .build()
+            )
         })
 
         txtTime = findViewById<TextView>(R.id.txtTime)
 
         val startBtn = findViewById<Button>(R.id.startBtn)
         startBtn.setOnClickListener(View.OnClickListener {
-            startTimer()
+            if (!timerFlag) {
+                startTimer()
+            }
         })
 
         val stopBtn = findViewById<Button>(R.id.stopBtn)
@@ -75,27 +82,53 @@ class TimerActivity : AppCompatActivity() {
 
 
     private fun startTimer() {
+        timerFlag = true
         timerTask = kotlin.concurrent.timer(period = 10) {
             time++
 
             val sec = time / 100
+            val min = sec / 60
             val milli = time % 100
 
+            var min_str : String = ""
+            if (min < 10) {
+                min_str = "0" + min.toString()
+            } else {
+                min_str = min.toString()
+            }
+
+            var sec_str : String = ""
+            var sec_mod = sec % 60
+            if (sec_mod < 10) {
+                sec_str = "0" + sec_mod.toString()
+            } else {
+                sec_str = sec_mod.toString()
+            }
+
+            var milli_str : String = ""
+            if (milli < 10) {
+                milli_str = "0" + milli.toString()
+            } else {
+                milli_str = milli.toString()
+            }
+
             runOnUiThread {
-                txtTime?.text = "${sec} : ${milli}"
+                txtTime?.text = "${min_str}:${sec_str}:${milli_str}"
             }
         }
     }
 
     private fun stopTimer() {
+        timerFlag = false
         timerTask?.cancel()
     }
 
     private fun resetTimer() {
+        timerFlag = false
         timerTask?.cancel()
 
         time = 0
-        txtTime?.text = "00:00"
+        txtTime?.text = "00:00:00"
     }
 
 }
